c9261af2da2f0e08af744d7082e3bc807b7f029b
chrys <294931812@qq.com>
2019-06-06 11:24:51 +0800

    轮播测试

--- src/layouts/app/Swiper/index.jsx
+++ src/layouts/app/Swiper/index.jsx
@@ -8,48 +8,51 @@ import Swiper from 'swiper/dist/js/swiper.min'
 import Cover from '../images/cover.jpg'
 
 class index extends React.Component {
-  componentDidMount() {
-    console.log(this.sildeRef.clientWidth)
+    componentDidMount() {
+        console.log(this.sildeRef.clientWidth)
 
-    this.swiper = new Swiper(this.wrapRef)
-  }
+        this.swiper = new Swiper(this.wrapRef, {
+            slidesPerView: 1.5,
+            centeredSlides: true
+        })
+    }
 
-  render() {
-    return <div className="swiper-container" ref={ref => this.wrapRef = ref}>
-      <div className="swiper-wrapper">
-        <div className="swiper-slide" ref={ref => this.sildeRef = ref}>
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
+    render() {
+        return <div className="swiper-container" ref={ref => this.wrapRef = ref}>
+            <div className="swiper-wrapper">
+                <div className="swiper-slide" ref={ref => this.sildeRef = ref}>
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+                <div className="swiper-slide">
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+                <div className="swiper-slide">
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+                <div className="swiper-slide">
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+                <div className="swiper-slide">
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+                <div className="swiper-slide">
+                    <div className="wrapper">
+                        <img src={Cover} alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="swiper-slide">
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
-        </div>
-        <div className="swiper-slide">
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
-        </div>
-        <div className="swiper-slide">
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
-        </div>
-        <div className="swiper-slide">
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
-        </div>
-        <div className="swiper-slide">
-          <div className="wrapper">
-            <img src={Cover} alt=""/>
-          </div>
-        </div>
-      </div>
-    </div>
-  }
+    }
 }
 
 export default index
